6b2f582455271167782cb63ab2e86cfd563ae44f
Lucas dos Santos Gomes <lucasdev.programador@gmail.com>
2023-08-21 22:11:06 -0300

    Criando getters and setters

--- Route.cs
+++ Route.cs
@@ -4,6 +4,33 @@ namespace TripProject {
     private double fuelValue; // Combustível
     private string tollValue; // Pedágio
 
-    
+    public double GetKmTraveled() {
+      return kmTraveled;
+    }
+
+    public void SetKmTraveled(double kmTraveled) {
+      this.kmTraveled = kmTraveled;
+    }
+
+    public double GetFuelValue() {
+      return fuelValue;
+    }
+
+    public void SetFuelValue(double fuelValue) {
+      this.fuelValue = fuelValue;
+    }
+
+    public string GetTollValue() {
+      return tollValue;
+    }
+
+    public void SetTollValue(string tollValue) {
+      this.tollValue = tollValue;
+    }
+
+
+    public Route(double km, double fuel, string toll) {
+      kmTraveled = km;
+    }
   }
 }
